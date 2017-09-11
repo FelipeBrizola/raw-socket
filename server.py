9ebb2f531de0ee81e051a3a22810ef9a09220b00
felipebrizola <felipe_brizola@hotmail.com>
2017-09-11 20:31:17 -0300

    recebendo pacote

--- server.py
+++ server.py
@@ -1,33 +1,25 @@
 # Echo server program
 import socket
 import sys
+import ethernet
 
-HOST = None               # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
+def rawsocket():
     try:
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        s = None
-        continue
-    try:
-        s.bind(sa)
-        s.listen(1)
-    except socket.error as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print 'could not open socket'
-    sys.exit(1)
-conn, addr = s.accept()
-print 'Connected by', addr
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.send(data)
-conn.close()
+        return socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+    except socket.error , msg:
+        print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+    
+
+if __name__ == "__main__":
+    sock = rawsocket()
+
+    while True:
+        network_data = sock.recv(2048)
+        encoded = network_data.encode('hex')
+        ethernetFrame = ethernet.openPackage(encoded)
+        print ethernetFrame['dstMac']
+        print ethernetFrame['srcMac']
+        print ethernetFrame['ethertype']
+        
+        print '\n'
