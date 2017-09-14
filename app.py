960c4537054e6eb85d0066f1039533e8f37fbc96
Rafael Julião <rafael.juliao.perfil@gmail.com>
2017-09-14 20:54:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 import socket
 import packet
 
-
-def send_sock():
+def create_socket():
     try:
-        #return socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-        #return socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-        return socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    except socket.error , msg:
-        print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    
-    
-def recv_socket():
-    try:
-        #return socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
         return socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-        #return socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
     except socket.error , msg:
         print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     
 
 def receiveUdpPackage(port):
-    sock = recv_socket()
+    sock = create_socket()
     receiving = True
     while receiving:
         network_data = sock.recv(2048)
@@ -34,11 +22,7 @@ def receiveUdpPackage(port):
 
 
 def sendUdpPackage(message, dest_ip, dest_port, src_port):
-    
     ethernet_packet = packet.build(message, dest_ip, dest_port, src_port)
-    
-    sock = send_sock()
-    sock.sendto(ethernet_packet, ("enp0s3", 0))
-    #sock.shutdown(socket.SHUT_RDWR)
-    #sock.close()
+    sock = create_socket()
+    sock.sendto(ethernet_packet.decode("hex"), ("enp4s0", 0))
 
