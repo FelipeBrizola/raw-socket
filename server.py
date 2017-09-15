df7617ce4fca43ca8d5f5ca064256cb90b445439
Vinicius Sesti <vinicius.s.sesti@gmail.com>
2017-09-14 21:15:09 -0300

    sleep

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import app
+import time
 
 if __name__ == "__main__":
-	(status, result) = app.receiveUdpPackage(50000)
-	print(status, result)
-	app.sendUdpPackage("FUCK YOU CLIENT PEACE OF SHIT|1", result['from_ip'], result['from_port'], 50000)
+	while(True):
+		(status, result) = app.receiveUdpPackage(50000)
+		print result['message'].decode("hex")
+		time.sleep(1)
+		app.sendUdpPackage("FUCK YOU CLIENT PEACE OF SHIT|1", result['from_ip'], result['from_port'], 50000)
 	
